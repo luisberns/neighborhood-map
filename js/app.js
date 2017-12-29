2eb4e25f5089d713185b6de4269d53b345978072
louisberns <louisuntitled@gmail.com>
2017-12-28 22:06:47 -0200

    Added initial ko and maps features

--- js/app.js
+++ js/app.js
@@ -1,37 +1,115 @@
 /*
- * Neightborhood Map Project - Udacity Front-end Nanodegree
- * Use Google Maps API and Knockout.js to display locations in a map and give a menu list with a filter
+ *  Neightborhood Map Project - Udacity Front-end Nanodegree
+ *  Use Google Maps API and Knockout.js to display locations in a map and give a menu list with a filter
  */
 
 // APPLICATION MVVM HANDLERS
-const model = {
-	locations: [
-		{title: "Someplace", location: {lat: -20, lng: 20}},
-		{title: "Someplace", location: {lat: -20, lng: 20}},
-		{title: "Someplace", location: {lat: -20, lng: 20}},
-		{title: "Someplace", location: {lat: -20, lng: 20}},
-		{title: "Someplace", location: {lat: -20, lng: 20}}
-	],
-	marker: {
+const locs = [
+		{id: 0, title: "France", location: {lat: 46.727106, lng: 2.465734}, info: "<div>Some really nice text on the info window</div>"},
+		{id: 1, title: "Amsterdam", location: {lat: 52.358717, lng: 4.868406}, info: "<div>Some really nice text on the info window</div>"},
+		{id: 2, title: "Prage", location: {lat: 50.071045, lng: 14.430600}, info: "<div>Some really nice text on the info window</div>"},
+		{id: 3, title: "Dublin", location: {lat: 53.324597, lng: -6.194791}, info: "<div>Some really nice text on the info window</div>"},
+		{id: 4, title: "Berlin", location: {lat: 52.528739, lng: 13.454198}, info: "<div>Some really nice text on the info window</div>"}
+]
 
+const viewModel = {
+	self: this,
+	query: ko.observable(''),
+	menuPane: ko.observable(null),
+	locations: ko.observableArray(),
+
+	toggleMenu: () => {
+		if (viewModel.menuPane() === null) {
+			viewModel.menuPane("menu-close");
+		} else {
+			viewModel.menuPane(null);
+		}
+	},
+	setCenterMap: loc => { map.setCenter(loc); },
+	showInfoWindow: (id) => {
+		infoWindow.map(i => {
+			i.setMap(null);
+		});
+		infoWindow[id].open(map, markers[id]);
+	},
+	showMarkerWindow: (id, loc) => {
+		viewModel.setCenterMap(loc);
+		viewModel.showInfoWindow(id);
 	}
 };
 
-const viewModel = {
+// Copy the value of locs to observableArray
+locs.map(l => viewModel.locations().push(l));
 
-};
+viewModel.searchList = ko.computed(() => {
+	let q = viewModel.query();
 
-const view = {
-
-};
-
+	if (!q) {
+		return viewModel.locations();
+	} else {
+		return ko.utils.arrayFilter(viewModel.locations(), item => {
+			return item.title.toLowerCase().indexOf(q) >= 0;
+		})
+	}
+});
 
 // GOOGLE MAPS API
-var map;
+var map,
+		markers = [],
+		infoWindow = [];
 
+// Initialize map on load
 function initMap() {
+
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: model.locations[0].location,
-		zoom: 15
+		center: viewModel.locations()[0].location,
+		zoom: 5,
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.LEFT_BOTTOM
+		}
 	});
+
+	// Responsive map by resizing window
+  google.maps.event.addDomListener(window, "resize", function() {
+      var center = map.getCenter();
+      google.maps.event.trigger(map, "resize");
+      map.setCenter(center);
+  });
+
+	marker = new google.maps.Marker({
+		position: viewModel.locations()[0].location,
+		title: 'Hello World!'
+	});
+
+	// Set initial MARKERS to the map
+	// use marker.setMap(map) to add one marker
+	viewModel.locations().map(item => {
+		let m = new google.maps.Marker({
+			id: item.id,
+			position: item.location,
+			title: item.title,
+		});
+
+		let iW = new google.maps.InfoWindow({
+			id: item.id,
+			content: item.info,
+			position: m.position
+		});
+
+		m.setMap(map);
+
+		// Populate MARKERS and INFOWINDOW arrays
+		markers.push(m);
+		infoWindow.push(iW);
+	})
+
+	// Add listener for open infoWindow and center map to marker on CLICK
+	markers.map(m => {
+		m.addListener('click', target => {
+			viewModel.showMarkerWindow(m.id, m.position);
+		});
+	});
+
+	// Aplly bindings to Knockout as soon as map is loaded
+	ko.applyBindings(viewModel);
 }
