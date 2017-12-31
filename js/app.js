c7d5dd1c60b01b4adbc654f2d92140d291b31970
louisberns <louisuntitled@gmail.com>
2017-12-31 20:10:00 -0200

    Added fetch requests with Foursquare API

--- js/app.js
+++ js/app.js
@@ -5,11 +5,11 @@
 
 // APPLICATION MVVM HANDLERS
 const locs = [
-		{id: 0, title: "Hardware Société", placeid: "ChIJpZjaLl1u5kcR0yN4kIoECqw", location: {lat: 48.8868764, lng: 2.344576800000027}, address: null, info: "<div>Some really nice text on the info window</div>"},
-		{id: 1, title: "La Caféothèque", placeid: "ChIJUZd-NP1x5kcRQhl91RYHxRs", location: {lat: null, lng: null}, address: null, info: "<div>Some really nice text on the info window</div>"},
-		{id: 2, title: "Fondation Café", placeid: "ChIJEfw8-gVu5kcRCoObsIY7C1I", location: {lat: null, lng: null}, address: null, info: "<div>Some really nice text on the info window</div>"},
-		{id: 3, title: "Bagelstein", placeid: "ChIJswHuekdu5kcR6xgWWkc5aPc", location: {lat: null, lng: null}, address: null, info: "<div>Some really nice text on the info window</div>"},
-		{id: 4, title: "CRAFT", placeid: "ChIJLeUyggtu5kcRNUgCe7GhauQ", location: {lat: null, lng: null}, address: null, info: "<div>Some really nice text on the info window</div>"}
+		{id: 0, title: "Hardware Société", placeid: "ChIJpZjaLl1u5kcR0yN4kIoECqw", fsId: "5710c77a498e3021c0641aa9", location: {lat: 48.8868764, lng: 2.344576800000027}, address: "10 Rue Lamarck, 75018 Paris, France", info: "<div>Some really nice text on the info window</div>"},
+		{id: 1, title: "La Caféothèque", placeid: "ChIJUZd-NP1x5kcRQhl91RYHxRs", fsId: "4bbb21a91261d13aa847eb98", location: {lat: 48.8544402, lng: 2.3557431000000406}, address: "52 Rue de l'Hôtel de ville, 75004 Paris, France", info: "<div>Some really nice text on the info window</div>"},
+		{id: 2, title: "Fondation Café", placeid: "ChIJEfw8-gVu5kcRCoObsIY7C1I", fsId: "527e5b6f11d2c8ad2cf37a22", location: {lat: 48.86570700000001, lng: 2.3615029999999706}, address: "16 Rue Dupetit-Thouars, 75003 Paris, France", info: "<div>Some really nice text on the info window</div>"},
+		{id: 3, title: "Bagelstein", placeid: "ChIJswHuekdu5kcR6xgWWkc5aPc", fsId: "546dd0fc498e1597e843450f", location: {lat: 48.8770224, lng: 2.338313400000061}, address: "8 Rue Saint-Lazare, 75009 Paris, France", info: "<div>Some really nice text on the info window</div>"},
+		{id: 4, title: "CRAFT", placeid: "ChIJLeUyggtu5kcRNUgCe7GhauQ", fsId: "50420756e4b0047a41a495fc", location: {lat: 48.87322330000001, lng: 2.363122100000055}, address: "24 Rue des Vinaigriers, 75010 Paris, France", info: "<div>Some really nice text on the info window</div>"}
 ]
 
 const viewModel = {
@@ -28,9 +28,12 @@ const viewModel = {
 	},
 	setCenterMap: loc => { map.setCenter(loc); },
 	showInfoWindow: id => {
+		let item = viewModel.locations()[id];
+
 		infoWindow.map(i => {
 			i.setMap(null);
 		});
+		infoWindow[id].setContent('<div>' + item.fsContent + '</div>' + item.info + '<div><strong>Address: </strong>' + item.address + '</div>')
 		infoWindow[id].open(map, markers[id]);
 	},
 	animateMarker: id => {
@@ -48,6 +51,35 @@ const viewModel = {
 
 // Copy the value of locs to observableArray
 locs.map(l => viewModel.locations().push(l));
+
+/*
+ * Resquest Foursquare API data
+ */
+function foursquareRequest() {
+	const client_id = 'O0R3FG5EKQ0JW4ND4DDLEBU11VOIKVNAKLTJPHY4M0A55S0Q',
+				client_secret = '1B44WB2ZAJZAU3X1ZFIZFMEXEJ2PVGCQWYWJNMTYQTYUJECF';
+
+	viewModel.locations().map(item => {
+		let i = item.fsId;
+		console.log(i);
+		fetch('https://api.foursquare.com/v2/venues/' + i + '?ll=' + viewModel.locations()[0].location.lat + ',' + viewModel.locations()[0].location.lng + '&client_id=' + client_id + '&client_secret=' + client_secret + '&v=20200101')
+		.then(resolve => {
+			console.dir(resolve);
+
+			resolve.json().then(data => {
+				console.log(data);
+
+				let h = data.response.venue.hours.status;
+				item.fsContent = h;
+			});
+
+
+			console.log(item);
+		}).catch(err => console.log(err));
+	});
+};
+
+foursquareRequest();
 
 // Check the viewport size to toggle menu on mobile
 if (viewModel.windowKo() <= 500) {
@@ -70,6 +102,7 @@ viewModel.searchList = ko.computed(() => {
 	}
 });
 
+// Filter markers as the user types
 viewModel.filterMarkers = ko.computed(() => {
 	var m = viewModel.searchList();
 
@@ -114,11 +147,12 @@ function initMap() {
 	// Set map after the geocode requests
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: viewModel.locations()[0].location,
-		zoom: 5,
+		zoom: 13,
 		mapTypeControlOptions: {
 			position: google.maps.ControlPosition.LEFT_BOTTOM
 		}
 	});
+	loadMap();
 }
 
 function loadMap() {
@@ -134,7 +168,6 @@ function loadMap() {
 
 		let iW = new google.maps.InfoWindow({
 			id: item.id,
-			content: item.info + '<div>' + item.address + '</div>',
 			position: m.position
 		});
 
@@ -155,44 +188,3 @@ function loadMap() {
 	// Aplly bindings to Knockout as soon as map is loaded
 	ko.applyBindings(viewModel);
 };
-
-// Get position and address info from geocoder
-const gCode = new Promise ((resolve, reject) => {
-	let resultsArray = [];
-	window.setTimeout(() => {
-		viewModel.locations().map(item => {
-			geocoder.geocode({'placeId' : item.placeid}, (results, status) => {
-				if (status === 'OK') {
-					console.log(results);
-					resultsArray.push(results);
-				} else {
-					window.alert('Geocoder failed due to: ' + status);
-					reject(Error(status));
-				}
-			});
-		});
-		resolve(resultsArray);
-	}, 1000);
-});
-
-
-// TODO: fix the damn promise info request
-var han,
-		hanT;
-gCode.then(results => {
-	console.log(results);
-	han = results;
-	viewModel.locations().map(item => {
-		hanT = item;
-		if (results[item.id] !== null) {
-			item.location.lat = results[item.id][0].geometry.location.lat();
-			item.location.lng = results[item.id][0].geometry.location.lng();
-			item.address = results[item.id][0].formatted_address;
-			return results;
-		} else {
-			window.alert('No results found');
-		}
-	})
-})
-.catch(err => console.log(err))
-.then(() => loadMap());
